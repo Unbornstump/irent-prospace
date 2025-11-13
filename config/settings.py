b3e4b252c7fc2a412218b0a7e46b96cd795b62d7
Unbornstump <warrenkagiri@gmail.com>
2025-11-13 03:14:05 +0300

    photos now served: MEDIA_URL + static() configured, admin retains full power

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [BASE_DIR / "listings/static"]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
